928f0972e371c073cc0522c24973b9f67de03b53
Rolando <rolandohernandezleal@gmail.com>
2025-09-14 15:35:47 -0300

    add overlay behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <div className="top-bar">
+        <span className="top-bar-counter">items in this list</span>
+        <button className='top-bar-button' onClick={() => setIsModalOpen(true)}>
+          New Item
+        </button>
+      </div>
+
+      {isModalOpen && <div className="modal-overlay" onClick={() => setIsModalOpen(false)}></div>}
+
+      <div className="list">
+      </div>
     </>
   )
 }
